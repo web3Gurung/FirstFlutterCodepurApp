f956839433cac76a351a4396c36e98a791a84d3f
web3Gurung <72032538+web3Gurung@users.noreply.github.com>
2023-12-17 02:07:51 +0530

    nothing coded, only learnt

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:open_app/widgets/drawer.dart';
 
+// day 11 - Learnt about context and contraints
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 

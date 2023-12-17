cf88b2b53d94f43e7d5e0ec02beafcefc7fde13d
web3Gurung <72032538+web3Gurung@users.noreply.github.com>
2023-12-18 03:03:57 +0530

    add catalog.json file and made homepage stateful

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:open_app/models/catalog.dart';
 import 'package:open_app/widgets/drawer.dart';
 import 'package:open_app/widgets/item_widget.dart';
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    var decodeData = jsonDecode(catalogJson);
+    print(decodeData);
+  }
 
   @override
   Widget build(BuildContext context) {
